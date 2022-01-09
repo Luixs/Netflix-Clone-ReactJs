fa483a1689c42d3e9feb5b8e24fbc27421afa808
Luixs <luis.guilherme009@gmail.com>
2022-01-09 08:05:19 -0300

    add all fectch functions

--- src/config/tmdb.js
+++ src/config/tmdb.js
@@ -28,39 +28,39 @@ export default () => {
                 items: await basicFech(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
             },
             {
-                slug: 'treding',
+                slug: 'trending',
                 title: 'Recomendado para Você',
-                items: []
+                items: await basicFech(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'toprated',
                 title: 'Em Alta',
-                items: []   
+                items: await basicFech(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'action',
                 title: 'Ação',
-                items: []
+                items: await basicFech(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'comedy',
                 title: 'Comédia',
-                items: []
+                items: await basicFech(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'horror',
                 title: 'Terror',
-                items: []
+                items: await basicFech(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'romance',
                 title: 'Romance',
-                items: []
+                items: await basicFech(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'documentary',
                 title: 'Documentários',
-                items: []
+                items: await basicFech(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
             }
         ];
     }
