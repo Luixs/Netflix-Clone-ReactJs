73f2b244a5fed8541f52ea01ea44c3f5079b9b6a
Luis Starlino <luis.guilherme009@gmail.com>
2022-01-09 11:28:54 -0300

    Create a FeatureMovie logistic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React, {useEffect, useState}from "react";
 import tmdb from "./config/tmdb";
 import './App.css'
+//-------------- Screen's 
 import MovieRow from "./components/movieRow/MovieRow";
+import FeatureMovie from "./components/featureMovie/FeatureMovie";
 
 export default ()=> {
 
   //When a reload the screen this fuction is gona run
-  const  [movieList, setMovieList] = useState([]);
+  const [movieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState(null);
+
   useEffect(()=>{
     //Use state to save the fetch list into a page
     const loadAll = async () => {
       //Taking all list
       let list = await tmdb.getHomeList();
       setMovieList(list)
+      //Taking a Feature Movie
+      let originals = list.filter(i=>i.slug === 'originals');
+      let ramdomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1 )) 
+      let chosen = originals[0].items.results[ramdomChosen]
+
+      console.log(chosen)
     }
     loadAll();
   }, []);
   
   return(
     <div className="page">
+      
+      {featureData && 
+        <FeatureMovie item={featureData}/>
+      }
+
       <section className="lists">
         {movieList.map((item, key)=>(
           <MovieRow key={key} title={item.title} items={item.items}/>
