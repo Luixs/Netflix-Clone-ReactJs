3ee5c292dfdc3dc0e7c63127887e27c8171760b9
Luis Starlino <luis.guilherme009@gmail.com>
2022-01-10 19:30:10 -0300

    All most done, making the ajusts

--- src/components/movieRow/movieRow.js
+++ src/components/movieRow/movieRow.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './MovieRow.css';
 import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 
 export default ({title, items}) => {
+
+    const [scroolX, setScroolX] = useState(-400);
+
+    const handleLeftArrow = () => {
+        let x = scroolX + Math.round(window.innerWidth / 2);
+        if(x > 0) {
+            x = 0;
+        }
+        setScroolX(x);
+    }
+    const handleRightArrow = () => {
+        let x = scroolX - Math.round(window.innerWidth / 2);
+        let listw = items.results.length * 150;
+        if((window.innerWidth - listw) > x ){
+            x = (window.innerWidth - listw) -60
+        }
+        setScroolX(x);
+    }
+    //------------------- 
     return(
         <div className='movieRow'>
             <h2>{title}</h2>
-            <div className='movieRow--left'>
+            <div className='movieRow--left' onClick={handleLeftArrow}>
                 <NavigateBeforeIcon style={{fontSize: 50 }}/>
             </div>
-            <div className='movieRow--rigth'>
+            <div className='movieRow--right' onClick={handleRightArrow}> 
                 <NavigateNextIcon style={{fontSize: 50 }}/>
             </div>
             <div className='movieRow--listArea'>
-                <div className='movieRow--list'>
+                <div className='movieRow--list' style={{
+                    marginLeft: scroolX,
+                    width: items.results.length * 150
+                }}>
                     {items.results.length > 0 && items.results.map((item, key)=>(
                         <div key={key} className='movieRow--item'>
                             <img src={`https://image.tmdb.org/t/p/w300/${item.poster_path}`}  alt={item.original_title}/>
