7aa5fc372f24b167c52fd186503eaf8fbd86dcad
Luis Starlino <luis.guilherme009@gmail.com>
2022-01-09 22:01:35 -0300

    Finish the all Header

--- src/App.js
+++ src/App.js
@@ -4,19 +4,26 @@ import './App.css'
 //-------------- Screen's 
 import MovieRow from "./components/movieRow/MovieRow";
 import FeatureMovie from "./components/featureMovie/FeaturedMovie";
+import Header from "./components/header/Header";
+//------------------
+
 
 export default ()=> {
 
-  //When a reload the screen this fuction is gona run
+  // ---------- CONST AREA ---------------
   const [movieList, setMovieList] = useState([]);
   const [featureData, setFeatureData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+  //--------------------------------------
 
+  //-------------- USE EFFECT AREA -------------------
   useEffect(()=>{
     //Use state to save the fetch list into a page
     const loadAll = async () => {
       //Taking all list
       let list = await tmdb.getHomeList();
       setMovieList(list)
+
       //Taking a Feature Movie
       let originals = list.filter(i=>i.slug === 'originals');
       let ramdomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1 )) 
@@ -28,10 +35,30 @@ export default ()=> {
     }
     loadAll();
   }, []);
-  
+
+  useEffect(()=>{
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setBlackHeader(true);
+      }else{
+        setBlackHeader(false);
+      }
+    }
+    //If exist ANY ONE scroll movimentation, we use the scroll Listener Fuction. This is a trigger.
+    window.addEventListener('scroll', scrollListener);
+    //Disnable the trigger
+    return ()=>{
+      window.removeEventListener('scroll',scrollListener);
+    }      
+  },[]);
+
+  //---------------------------------------------
+  //-------------- RENDER APP -------------------
   return(
     <div className="page">
       
+      <Header black={blackHeader}/>
+
       {featureData && 
         <FeatureMovie item={featureData}/>
       }
